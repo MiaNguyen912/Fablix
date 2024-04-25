35c2597085c970803ab3d38492bc4a26f6240c40
Daniel Lee <daniebl1@uci.edu>
2024-04-24 19:43:13 -0700

    Add cart page
    - Added html and js for frontend, handling getting the cart info from the session
    - Added cartservlet to handle API calls to return the movie title and price of each movie

---
+++ src/CartServlet.java
@@ -0,0 +1,114 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+
+
+// Declaring a WebServlet called MoviesServlet, which maps to url "/api/20movies"
+@WebServlet(name = "CartServlet", urlPatterns = "/api/cart")
+public class CartServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    // Create a dataSource which registered in web.
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("application/json"); // Response mime type
+
+        // Output stream to STDOUT
+        PrintWriter out = response.getWriter();
+
+        // Get a connection from dataSource and let resource manager close the connection after usage.
+        try (Connection conn = dataSource.getConnection()) {
+
+            // Declare our statement
+            Statement statement = conn.createStatement();
+
+            // String of movieIds separated by , should be passed in, parse through it into a movieId array, then
+            // run an sql query where it only returns movies that are in that array?
+            String movieIdsString = request.getParameter("movieIds");
+            // Now just get the movie that match the id
+
+            // Split the movie IDs string into an array of individual IDs
+            String[] movieIdsArray = movieIdsString.split(",");
+
+            StringBuilder queryBuilder = new StringBuilder("SELECT * FROM movies WHERE id IN (");
+            for (int i = 0; i < movieIdsArray.length; i++) {
+                queryBuilder.append("'").append(movieIdsArray[i]).append("'");
+                if (i < movieIdsArray.length - 1) {
+                    queryBuilder.append(",");
+                }
+            }
+            queryBuilder.append(")");
+
+            // Perform the query
+            ResultSet rs = statement.executeQuery(queryBuilder.toString());
+
+            JsonArray jsonArray = new JsonArray();
+
+            // Iterate through each row of rs
+            while (rs.next()) {
+                String movie_id = rs.getString("id");
+                String title = rs.getString("title");
+                int price = rs.getInt("price");
+                // Create a JsonObject based on the data we retrieve from rs
+                JsonObject jsonObject = new JsonObject();
+                jsonObject.addProperty("movie_id", movie_id);
+                jsonObject.addProperty("movie_title", title);
+                jsonObject.addProperty("movie_price", price);
+
+                jsonArray.add(jsonObject);
+            }
+            rs.close();
+            statement.close();
+
+            // Log to localhost log
+            request.getServletContext().log("getting " + jsonArray.size() + " results");
+
+            // Write JSON string to output
+            out.write(jsonArray.toString());
+            // Set response status to 200 (OK)
+            response.setStatus(200);
+
+        } catch (Exception e) {
+
+            // Write error message JSON object to output
+            JsonObject jsonObject = new JsonObject();
+            jsonObject.addProperty("errorMessage", e.getMessage());
+            out.write(jsonObject.toString());
+
+            // Set response status to 500 (Internal Server Error)
+            response.setStatus(500);
+        } finally {
+            out.close();
+        }
+
+        // Always remember to close db connection after usage. Here it's done by try-with-resources
+
+    }
+}
