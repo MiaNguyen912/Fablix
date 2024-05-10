bc6902c5cf9692688a4884c3fed219b3aa9920b2
Daniel Lee <danielbolee@gmail.com>
2024-05-09 19:41:20 -0700

    Edit captcha variables

--- src/LoginServlet/RecaptchaConstants.java
+++ src/LoginServlet/RecaptchaConstants.java
@@ -1,7 +1,7 @@
 package LoginServlet;
 
 public class RecaptchaConstants {
-    public static final String SECRET_KEY ="6Lf5YswpAAAAALOBL5VJgKBAQjCYMAsZEvLlynYR";
+    public static final String SECRET_KEY ="6Lc5gdcpAAAAABeXOyhNpllZLtcB8qY_ThmbFcld";
     // open in incognito mode to see the images verification pop up
     // otherwise, it is just "im not robot" check box
 }
