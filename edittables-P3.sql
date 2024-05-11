acf5e8dcfb32f10b958f1a1bd235ab7c615ee2dc
Daniel Lee <danielbolee@gmail.com>
2024-05-10 19:05:13 -0700

    Update edittables to include logic for stars_in_movies and genres_in_movies

--- edittables-P3.sql
+++ edittables-P3.sql
@@ -8,11 +8,12 @@ CREATE TABLE IF NOT EXISTS employees(
 INSERT INTO employees (email, password, fullname) VALUES ("classta@email.edu", "classta", "andTA CS122B");
 
 
-CREATE TABLE id_manager (
+CREATE TABLE IF NOT EXISTS id_manager (
                             table_name VARCHAR(50) NOT NULL,
                             next_id INT NOT NULL,
                             PRIMARY KEY (table_name)
 );
+
 
 INSERT INTO id_manager (table_name, next_id) SELECT 'stars', IFNULL(MAX(CAST(SUBSTRING(id, 3) AS UNSIGNED)) + 1, 1) FROM stars;
 
@@ -21,6 +22,7 @@ INSERT INTO id_manager (table_name, next_id) SELECT 'movies', IFNULL(MAX(CAST(SU
 
 DELIMITER //
 
+DROP PROCEDURE IF EXISTS add_star;
 CREATE PROCEDURE `add_star`(
     IN in_name VARCHAR(100),
     IN in_year INT,
@@ -46,6 +48,8 @@ BEGIN
 
 COMMIT;
 
+
+DROP PROCEDURE IF EXISTS add_movie;
 CREATE PROCEDURE `add_movie`(
     IN in_title VARCHAR(100),
     IN in_director VARCHAR(100),
@@ -60,7 +64,6 @@ CREATE PROCEDURE `add_movie`(
 BEGIN
     DECLARE next_movie_id INT;
     DECLARE existing_genre_id INT;
-    DECLARE existing_star_id VARCHAR(10);
 
 
     -- Check if the movie already exists, matching title year and director
@@ -75,7 +78,7 @@ BEGIN
         SELECT next_id INTO next_movie_id FROM id_manager WHERE table_name = 'movies' FOR UPDATE;
 
         -- Construct the new movie ID with the 'mv' prefix
-        SET out_movie_id = CONCAT('mv', next_movie_id);
+        SET out_movie_id = CONCAT('tt', next_movie_id);
 
         -- Insert the new movie with the generated ID
         INSERT INTO movies (id, title, director, year) VALUES (out_movie_id, in_title, in_director, in_year);
@@ -83,29 +86,37 @@ BEGIN
         -- Increment the next_id in id_manager
         UPDATE id_manager SET next_id = next_id + 1 WHERE table_name = 'movies';
 
+
+        -- Check if the genre already exists, and insert if it doesnt'
+        SELECT id INTO existing_genre_id FROM genres WHERE name = in_genre LIMIT 1;
+        IF existing_genre_id IS NULL THEN
+                -- Insert new genre and get the new ID
+                INSERT INTO genres (name) VALUES (in_genre);
+                -- Since auto generated, can get new id using last_insert_id()
+                SET out_genre_id = LAST_INSERT_ID();
+        ELSE
+                -- Set out_genre_id to the existing genre's ID
+                SET out_genre_id = existing_genre_id;
+        END IF;
+
+        -- Insert into genres_in_movies
+        INSERT INTO genres_in_movies (genreId, movieId) VALUES (out_genre_id, out_movie_id);
+
+
+        -- Now call add_star to handle adding a star
+        CALL add_star(in_star_name, NULL, out_star_id);
+
+        -- Insert into stars_in_movies
+        INSERT INTO stars_in_movies (starId, movieId) VALUES (out_star_id, out_movie_id);
+
+        SET out_status = 'Movie added correctly';
+
         COMMIT;
     ELSE
         -- Duplicate movie, Return with no action taken
         SET out_status = 'Movie already exists';
-        RETURN;
+
     END IF;
-
-
-    -- Check if the genre already exists
-    SELECT id INTO existing_genre_id FROM genres WHERE name = in_genre LIMIT 1;
-    IF existing_genre_id IS NULL THEN
-        -- Insert new genre and get the new ID
-        INSERT INTO genres (name) VALUES (in_genre);
-        SET out_genre_id = LAST_INSERT_ID();
-    ELSE
-        -- Set out_genre_id to the existing genre's ID
-        SET out_genre_id = existing_genre_id;
-    END IF;
-
-    -- Now call add_star to handle adding a star
-    CALL add_star(in_star_name, NULL, out_star_id);
-
-    SET out_status = 'Movie added correctly';
 
 END //
 
