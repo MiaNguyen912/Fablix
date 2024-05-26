0e389e3dba48a20b0c77c431602503a85fc47145
Your Name <daniebl1@uci.edu>
2024-05-26 02:57:58 +0000

    Update recaptcha for new ips

--- src/LoginServlet/RecaptchaConstants.java
+++ src/LoginServlet/RecaptchaConstants.java
@@ -1,7 +1,7 @@
 package LoginServlet;
 
 public class RecaptchaConstants {
-    public static final String SECRET_KEY ="6LekkuUpAAAAAE990uZBOQl76EljvSlPCuR50amL"; //6Lc5gdcpAAAAABeXOyhNpllZLtcB8qY_ThmbFcld
+    public static final String SECRET_KEY ="6Lc5gdcpAAAAABeXOyhNpllZLtcB8qY_ThmbFcld"; //6Lc5gdcpAAAAABeXOyhNpllZLtcB8qY_ThmbFcld
     // open in incognito mode to see the images verification pop up
     // otherwise, it is just "im not robot" check box
 }
